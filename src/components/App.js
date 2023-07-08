21c3c5ee35a73c8737494bc0826f04cfacad38b1
Ryan Staats <rstaats0927@gmail.com>
2023-07-08 14:49:26 -0500

    created files for Item component

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,10 @@ function App() {
       <Header></Header>
       <main>
         <Weather day={true} type='sunny'></Weather>
-        <section className='Cards'>Cards</section>
+        <section className='Cards'>
+        <p>Today is 75&#176;F / You may want to wear:</p>
+        <ul></ul>
+        </section>
       </main>
     </div>
   );
