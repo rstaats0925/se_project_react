8c34e2dece90e73fc2b00dcc1a4ef109f495f665
Ryan Staats <rstaats0927@gmail.com>
2023-07-09 12:11:12 -0500

    created folder for App & Main

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import React from 'react';
-import Header from './Header/Header.js';
-import Weather from './Weather/Weather.js';
-import ItemCard from './ItemCard/ItemCard.js';
-import defaultClothingItems from '../utils/utils.js';
+import Header from '../Header/Header.js';
+import Weather from '../Weather/Weather.js';
+import ItemCard from '../ItemCard/ItemCard.js';
+import defaultClothingItems from '../../utils/utils.js';
 
 function App() {
   const [items, setItems] = React.useState(defaultClothingItems);
