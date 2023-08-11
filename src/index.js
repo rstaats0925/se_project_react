1bfcc92a53aa89700b5540cba7c9f4dd3d7aeb03
Ryan Staats <rstaats0927@gmail.com>
2023-08-10 19:57:56 -0500

    moved index files back to src

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './src/components/App/App.js';
-import reportWebVitals from './src/reportWebVitals';
+import App from './components/App/App.js';
+import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
