3b553c52f1683634eed5259bb965b9eec9aed7ee
Ryan Staats <rstaats0927@gmail.com>
2023-08-24 17:50:54 -0500

    created delete button for itemModal

--- src/components/ItemModal/ItemModal.js
+++ src/components/ItemModal/ItemModal.js
@@ -5,9 +5,10 @@ function ItemModal ({selectedCard, onClose}) {
     <div className="modal ItemModal">
       <div className="modal__content ItemModal__content">
       <button className='ItemModal__close-button' type='button' onClick={onClose}></button>
-        <img className="ItemModal__image" src={selectedCard.link} alt={selectedCard.name}/>
-        <p className="ItemModal__item-name">{selectedCard.name}</p>
-        <p className="ItemModal__weather-type">Weather: {selectedCard.weather}</p>
+      <img className="ItemModal__image" src={selectedCard.link} alt={selectedCard.name}/>
+      <p className="ItemModal__item-name">{selectedCard.name}</p>
+      <p className="ItemModal__weather-type">Weather: {selectedCard.weather}</p>
+      <button className='ItemModal__delete' type='button'>Delete item</button>
       </div>
     </div>
   )
