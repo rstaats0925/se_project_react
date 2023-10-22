ab57f367623c3b88e0bd7904f26f7a18d14ad3b1
Ryan Staats <rstaats0927@gmail.com>
2023-10-22 17:38:32 -0500

    most of the html & css for login modal are finished. Still need the register link.

--- src/components/LoginModal/LoginModal.js
+++ src/components/LoginModal/LoginModal.js
@@ -3,9 +3,32 @@ import ModalWithForm from '../ModalWithForm/ModalWithForm';
 import './LoginModal.css';
 
 function LoginModal({onClose}) {
-  <ModalWithForm name="signUp" buttonText="Next" title="Sign Up" onClose={onClose}>
-    
-  </ModalWithForm>
+  return (
+    <ModalWithForm name="signUp" buttonText="Next" title="Log in" onClose={onClose}>
+      <div className='form'>
+        <div className='form__input-block'>
+          <label htmlFor='email' className='input-block__label'>Email</label>
+          <input
+          id='email'
+          name='email'
+          type='email'
+          placeholder='Email'
+          className='input-block__input'
+          />
+        </div>
+        <div className='form__input-block'>
+          <label htmlFor='password' className='input-block__label'>Password</label>
+          <input
+          type='password'
+          id='password'
+          name='password'
+          placeholder='Password'
+          className='input-block__input'
+           />
+        </div>
+      </div>
+    </ModalWithForm>
+  )
 }
 
 export default LoginModal;
